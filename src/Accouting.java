e27c323a8cebc9f162b603a35e274639c751f3c2
ArturD <artur2134@mail.ru>
2023-10-22 14:38:29 +0300

    Добавил консольное меню, добавил класс, добавил метод startApp

---
+++ src/Accouting.java
@@ -0,0 +1,14 @@
+public class Accouting {
+    public static void startApp() {
+        printMenu();
+    }
+
+    public static void printMenu() {
+        System.out.println("1. Считать все месячные отчеты");
+        System.out.println("2. Считать годовой отчет");
+        System.out.println("3. Сверить отчеты");
+        System.out.println("4. Вывести информацию о всех месячных отчетах");
+        System.out.println("5. Вывести информацию о всех месячных отчетах");
+        System.out.println("6. Выход из приложения");
+    }
+}
