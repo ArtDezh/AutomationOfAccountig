1ffabae1210bf9a45651d6e1e4fd580af36b205e
ArturD <artur2134@mail.ru>
2023-10-22 19:48:28 +0300

    Появился метод считывания файлов, дополнил switch

--- src/MonthlyReport.java
+++ src/MonthlyReport.java
@@ -1,3 +1,45 @@
 public class MonthlyReport {
+    private String itemName;
+    private boolean isExpense;
+    private int quantity;
+    private int sumOfOne;
 
+    public MonthlyReport(String itemName, boolean isExpense, int quantity, int sumOfOne) {
+        this.itemName = itemName;
+        this.isExpense = isExpense;
+        this.quantity = quantity;
+        this.sumOfOne = sumOfOne;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public boolean isExpense() {
+        return isExpense;
+    }
+
+    public void setExpense(boolean expense) {
+        isExpense = expense;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getSumOfOne() {
+        return sumOfOne;
+    }
+
+    public void setSumOfOne(int sumOfOne) {
+        this.sumOfOne = sumOfOne;
+    }
 }
