e1aeb2e110a8f8867bc56165d20e44433984da88
ArturD <artur2134@mail.ru>
2023-10-22 20:16:03 +0300

    Дополнил класс годового отчета и файл с вход.  данными

--- src/YearlyReport.java
+++ src/YearlyReport.java
@@ -1,3 +1,35 @@
 public class YearlyReport {
+    private String month;
+    private int amount;
+    private boolean is_expense;
 
+    public YearlyReport(String month, int amount, boolean is_expense) {
+        this.month = month;
+        this.amount = amount;
+        this.is_expense = is_expense;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public boolean isIs_expense() {
+        return is_expense;
+    }
+
+    public void setIs_expense(boolean is_expense) {
+        this.is_expense = is_expense;
+    }
 }
